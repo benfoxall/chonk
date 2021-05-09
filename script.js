9b05ba8701f74b8fa36e1aae80baa3ea67a61da0
Ben Foxall <benfoxall@gmail.com>
2021-05-09 22:53:30 +0000

    Tweaks

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
+import humanFormat from 'https://cdn.skypack.dev/human-format';
 
-const input = document.querySelector('input')
+const input = document.querySelector("input");
 
-input.addEventListener('input', start)
+
+input.addEventListener("input", start);
 
 async function start() {
   const [file] = input.files;
 
-  const stream = file.stream()
+  const stream = file.stream();
   const reader = stream.getReader();
 
   const start = performance.now();
 
-  let count = 0
-  let size = 0
+  let count = 0;
+  let size = 0;
 
   while (true) {
-    const { done, value } = await reader.read()
+    const { done, value } = await reader.read();
     if (done) break;
 
-    size += value.byteLength
+    size += value.byteLength;
     count++;
   }
 
   const end = performance.now();
 
-  console.log(size, count, end - start)
+  const _size = humanFormat(size);
 
-  document.querySelector
-    ('#size output').innerText = `
-     ${new Intl.NumberFormat('en', { style: 'unit', unit: 'byte' }).format(size)}, over ${count} chunks, read in ${new Intl.NumberFormat('en', { style: 'unit', unit: 'millisecond' }).format(end - start)
-    }
-    `
+  const _time = new Intl.NumberFormat("en", {
+    style: "unit",
+    unit: "millisecond",
+  }).format(end - start);
 
-
-  console.log(new Intl.NumberFormat('en', { style: 'unit', unit: 'byte' }).format(size));
-
+  document.querySelector(
+    "#size output"
+  ).innerText = `${_size}, over ${count} chunks, read in ${_time}`;
 }
